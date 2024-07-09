bc5789a1c9c0c9fc8aae1ac94941e2d14708ced7
prathambhalke <prathameshbhalke125@gmail.com>
2024-07-09 11:10:55 +0530

    chapter 9 streaming

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,24 +16,30 @@ const iconMap = {
 
 export default async function CardWrapper() {
   const fetchdata =  await fetchCardData();
-  const {
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  //   numberOfInvoices,
+  //   numberOfCustomers
+  // } = fetchdata
+    const {
+    numberOfInvoices,
+    numberOfCustomers,
     totalPaidInvoices,
     totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers
-  } = fetchdata
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
